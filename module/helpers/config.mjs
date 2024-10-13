932e2a382bce6db9316dc364f5d51afe70241db9
Igor Ponomarev <misterzicold@gmail.com>
2024-10-13 15:08:50 +0700

    initital commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const NUMENERA_YZE = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+NUMENERA_YZE.abilities = {
+  str: 'NUMENERA_YZE.Ability.Str.long',
+  dex: 'NUMENERA_YZE.Ability.Dex.long',
+  con: 'NUMENERA_YZE.Ability.Con.long',
+  int: 'NUMENERA_YZE.Ability.Int.long',
+  wis: 'NUMENERA_YZE.Ability.Wis.long',
+  cha: 'NUMENERA_YZE.Ability.Cha.long',
+};
+
+NUMENERA_YZE.abilityAbbreviations = {
+  str: 'NUMENERA_YZE.Ability.Str.abbr',
+  dex: 'NUMENERA_YZE.Ability.Dex.abbr',
+  con: 'NUMENERA_YZE.Ability.Con.abbr',
+  int: 'NUMENERA_YZE.Ability.Int.abbr',
+  wis: 'NUMENERA_YZE.Ability.Wis.abbr',
+  cha: 'NUMENERA_YZE.Ability.Cha.abbr',
+};
